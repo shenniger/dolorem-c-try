49b7b7d306c0a2f494ef40ba86ce1ae38175978d
Simon Henniger <git@simonhenniger.info>
2022-10-08 20:15:31 +0200

    .

--- rlpl.sh
+++ rlpl.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-
+clear
+echo Welcome to Try Dolorem-C! Preparing the machine for you...
+curl https://shenniger.github.io/try-dolorem-c/dolorem-c.tar.bz2 | tar xjf -
+cd dolorem-c
+clear
+echo Launching RLPL...
+LD_LIBRARY_PATH=. ./dolorem rlpl.dlr
